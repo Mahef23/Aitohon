765e083ee2ecec1a06bc13806ef4702bbb9d1b83
Mahef23 <monkeygidr0@gmail.com>
2024-10-17 21:27:39 +0300

    1710

--- udaka1.py
+++ udaka1.py
@@ -1,6 +1,7 @@
 
-nom = input("Quel est votre nom")
-age = input("Quel est votre age")
+nom = input("Quel est votre nom? ")
+age = input("Quel est votre age? ")
+profession = input("Quel est votre profession? ")
 try:
     agei= int(age)+1
 except:
@@ -8,3 +9,5 @@ except:
 else:
     print(f"Vous vous appelez {nom} et vous avez {age} ans")
     print(f"Donc l'année prochaine vous avez {agei}")
+
+print(f"Et vous travaillez dans le domaine {profession}")

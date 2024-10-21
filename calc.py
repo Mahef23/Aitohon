b39922f01e87ba8460bb7f8dc5bc873cf6529b62
Mahef23 <monkeygidr0@gmail.com>
2024-10-21 17:06:08 +0530

    2110

---
+++ calc.py
@@ -0,0 +1,13 @@
+def mark(res):
+    if res<=60 : 
+        print("Fail")
+    elif  res>60 and res<=70:
+        print("Pass")
+    else: 
+        print("Excellent")
+    return 
+
+def average(point):
+    isa = point *5
+    return isa
+

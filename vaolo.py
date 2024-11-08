31f2935380b7db44516e202d8a564d62a36f6482
Mahef23 <monkeygidr0@gmail.com>
2024-11-08 15:58:28 +0530

    0711

--- vaolo.py
+++ vaolo.py
@@ -1,6 +1,15 @@
-a = 5
-b = 6
+nom = input("Name : ")
+math= input("Note math: ")
+svt=input("Note Science: ")
+physic = input("Note Physique: ")
 
-average = float((a+b)/2)
-print(average)
+moyenne = (int(math) + int(svt) + int(physic))/3
+
+
+if moyenne< 60:
+    print(f"{nom}, you Failed . MARK= {moyenne}")
+elif moyenne>=60 and moyenne<70:
+    print(f"{nom}, you Passed . MARK= {moyenne}")
+else:
+    print(f"{nom},Good . MARK= {moyenne}")
 

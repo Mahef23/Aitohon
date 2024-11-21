dbac4d702e6481759e969a8f6d14a3097d2e6ab0
Mahef23 <monkeygidr0@gmail.com>
2024-11-21 11:46:35 +0530

    1121

--- UdakaWHile.py
+++ UdakaWHile.py
@@ -3,7 +3,12 @@ while n<10:
     print(n)
     n=n+1"""
 
-password = input("ENtrer le mot de passe : ")
+"""password = input("ENtrer le mot de passe : ")
 while not password == "TAY":
     password =input("Veuillez entrez le bon Mot de passe : ")
-print("***BIENVENUE***")
+print("***BIENVENUE***")"""
+
+reponse = input("Entrer votre reponse : ")
+while not reponse == "78":
+    reponse = input("Faux. Entrer votre reponse : ")
+print("Vrai")
---
+++ revy.py
@@ -0,0 +1,12 @@
+vera = 0
+toaka = True
+toakaL = 2
+def matsaka(litre):
+    toakaL += litre
+
+if vera == 0 :
+    if toaka == True:
+        vera += 1
+    elif
+    else :
+        matsaka(5)

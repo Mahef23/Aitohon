c333a1f4f81803b433d146b2742fa44970265c9c
Mahef23 <monkeygidr0@gmail.com>
2024-11-24 18:45:58 +0530

    1124

--- revy.py
+++ revy.py
@@ -1,12 +1,15 @@
-vera = 0
-toaka = True
-toakaL = 2
+vera= 0
+toakaL= 2
 def matsaka(litre):
-    toakaL += litre
+    global toakaL
+    toakaL +=litre
+    print("Matsaka")
 
 if vera == 0 :
-    if toaka == True:
-        vera += 1
-    elif
-    else :
-        matsaka(5)
+    vera +=2
+    toakaL -=2
+    if toakaL == 0:
+        while  toakaL < 15:
+            matsaka(2)
+    else:
+        print("Mitohy ny revy")

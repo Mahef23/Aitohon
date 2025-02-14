3f0a52fbf4ad26c1d56b5c2ac651755a5536c4ae
Mahef23 <monkeygidr0@gmail.com>
2025-02-15 00:56:25 +0530

    "1402"

---
+++ JeuDM/epreuvescode2.py
@@ -0,0 +1,20 @@
+import time
+def transform(temps):
+    while temps>=60 :
+        temps-=60
+        global minutes
+        minutes+=1
+        global secondes 
+        secondes= temps
+    print(f"{minutes} : {secondes}")
+    print()
+temps=int(input("Entrer le temps: "))
+minutes=0
+secondes=0
+transform(temps)
+for i in range (minutes,-1,-1):
+    for j in range(secondes,-1,-1):
+        time.sleep(0.2)
+        print(f"{i} : {j}" , flush=True)
+
+    secondes=60
